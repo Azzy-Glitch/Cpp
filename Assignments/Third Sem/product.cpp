35a7271b26fb2d15dfb739dba6b694d34ee38832
Abdul Aziz <aapppp742@gmail.com>
2025-12-13 09:52:34 +0500

    Update Cpp

---
+++ Assignments/Third Sem/product.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+class Product
+{
+    string pname;
+    string pcolor;
+    string psize;
+    double price;
+    void get_data();
+    void display_data();
+
+public:
+    Product()
+    {
+        get_data();
+        display_data();
+    }
+};
+
+void Product ::get_data()
+{
+    cout << "Enter Product Name ";
+    cin >> pname;
+    cout << "Enter Product Color ";
+    cin >> pcolor;
+    cout << "Enter Product Size ";
+    cin >> psize;
+    cout << "Enter Product price ";
+    cin >> price;
+}
+
+void Product ::display_data()
+{
+    cout << "Enter Product Name " << pname;
+    cout << "Enter Product Color " << pcolor;
+    cout << "Enter Product Size " << psize;
+    cout << "Enter Product price " << price;
+}
+
+int main()
+{
+    Product p;
+    return 0;
+}
