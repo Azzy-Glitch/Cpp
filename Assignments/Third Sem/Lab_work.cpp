ba11610d1354b5e6f67f354b10402d3672b00a08
Abdul Aziz <aapppp742@gmail.com>
2026-01-31 10:11:06 +0500

    Update Lab_work.cpp

--- Assignments/Third Sem/Lab_work.cpp
+++ Assignments/Third Sem/Lab_work.cpp
@@ -1,43 +1,57 @@
 // Write a cpp program which acts like a basic AtM
-// it must have the following Functions 
-// Deposit Amount 
-// WithDraw Allow Rs 2000 Per Transaction 
-// Display Amount 
+// it must have the following Functions
+// Deposit Amount
+// WithDraw Allow Rs 2000 Per Transaction
+// Display Amount
 
 #include <iostream>
 using namespace std;
 
-class ATM{
+class ATM
+{
     float balance;
-    public:
-    ATM(){
+
+public:
+    ATM()
+    {
         balance = 0;
     }
-    void deposit(float amount){
+
+    void deposit(float amount)
+    {
         balance += amount;
         cout << "Deposited: Rs " << amount << endl;
     }
-    void withdraw(float amount){
-        if(amount > 2000){
+
+    void withdraw(float amount)
+    {
+        if (amount > 2000)
+        {
             cout << "Withdrawal limit exceeded. You can withdraw up to Rs 2000 per transaction." << endl;
-        } else if(amount > balance){
+        }
+        else if (amount > balance)
+        {
             cout << "Insufficient balance." << endl;
-        } else {
+        }
+        else
+        {
             balance -= amount;
             cout << "Withdrew: Rs " << amount << endl;
         }
     }
-    void displayBalance(){
+    void displayBalance()
+    {
         cout << "Current Balance: Rs " << balance << endl;
     }
-
 };
-int main(){
+int main()
+{
     ATM myATM;
     int choice;
     float amount;
 
-    do{
+    do
+    {
         cout << "\nATM Menu:\n";
         cout << "1. Deposit Amount\n";
         cout << "2. Withdraw Amount\n";
@@ -46,28 +60,28 @@ int main(){
         cout << "Enter your choice: ";
         cin >> choice;
 
-        switch(choice){
-            case 1:
-                cout << "Enter amount to deposit: Rs ";
-                cin >> amount;
-                myATM.deposit(amount);
-                break;
-            case 2:
-                cout << "Enter amount to withdraw: Rs ";
-                cin >> amount;
-                myATM.withdraw(amount);
-                break;
-            case 3:
-                myATM.displayBalance();
-                break;
-            case 4:
-                cout << "Exiting ATM. Thank you!" << endl;
-                break;
-            default:
-                cout << "Invalid choice. Please try again." << endl;
+        switch (choice)
+        {
+        case 1:
+            cout << "Enter amount to deposit: Rs ";
+            cin >> amount;
+            myATM.deposit(amount);
+            break;
+        case 2:
+            cout << "Enter amount to withdraw: Rs ";
+            cin >> amount;
+            myATM.withdraw(amount);
+            break;
+        case 3:
+            myATM.displayBalance();
+            break;
+        case 4:
+            cout << "Exiting ATM. Thank you!" << endl;
+            break;
+        default:
+            cout << "Invalid choice. Please try again." << endl;
         }
-    } while(choice != 4);
+    } while (choice != 4);
 
     return 0;
-
 }
